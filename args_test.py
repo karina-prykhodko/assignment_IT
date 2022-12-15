51a17db13b28ab80a85ca7b672731d43a791b4e3
karina-prykhodko <115088236+silwersty@users.noreply.github.com>
2022-12-15 10:02:37 +0100

    add structure

--- args_test.py
+++ args_test.py
@@ -4,6 +4,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('infile', type=argparse.FileType('r'))
 parser.add_argument('-medals', dest='medals', nargs=2, required=True)
 parser.add_argument('-output', '--output', type=argparse.FileType('w', encoding='UTF-8'))
+parser.add_argument('-year', dest='year' , nargs=1, type= int )
+parser.add_argument('-country', dest='country', nargs=1, type= str)
 
 args = parser.parse_args()
 print(args.medals)
@@ -19,3 +21,15 @@ with args.infile as file:
 
 if args.output is not None:
   args.output.writelines(f'{store[0]}\n')
+
+def medalist(file_name, country, year):
+  pass
+
+def total_medals(medalist):
+  pass 
+
+def country_year(file_name, country, year):
+  pass 
+
+def first_ten(medalists):
+  pass
