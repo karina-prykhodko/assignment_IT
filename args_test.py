6e5a2568051e2c19ab5f457d4c4c20cab33f886b
karina-prykhodko <115088236+silwersty@users.noreply.github.com>
2022-12-21 13:15:44 +0100

    final

--- args_test.py
+++ args_test.py
@@ -1,113 +1,100 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('infile', type=argparse.FileType('r'))
-parser.add_argument('-medals', dest='medals', nargs=2, required=True)
+parser.add_argument('infile')
+parser.add_argument('-medals', dest='medals', nargs=2)
 parser.add_argument('-output', '--output', type=argparse.FileType('w', encoding='UTF-8'))
-parser.add_argument('-year', dest='year' , nargs=1, type= int )
-parser.add_argument('-country', dest='country', nargs=1, type= str)
 
 args = parser.parse_args()
-print(args.medals)
 
-
-with args.infile as file:
-  next_line = file.readline()
-  store = [next_line]
-  while next_line:
-      # do stuff with line
-      next_line = file.readline()
-      store.append(next_line)
-
-if args.output is not None:
-  args.output.writelines(f'{store[0]}\n')
-
-
-def medalist(file_name, country, year):
+def medalist(country, year):
   medalists = []
-
-  with open(file_name) as file:
+  file = args.infile
+  with open(file , 'r') as file:
     next_line = file.readline()
-
     for line in file:
-
-      part = line.split('/t')
-
+      part = line.split('\t')
       medal = part[14].strip()
       annum = int(part[9].strip())
       team = part[6].strip()
-      noc = part[7].stip()
-
-      if medal != 'NA' and annum == year in (team, noc):
+      noc = part[7].strip()
+      if medal != 'NA' and annum == year and country in (team, noc):
         medalists.append(part)
-
   return medalists
 
 
 def total_medals(medalists):
-  golg = 0
+  gold = 0
   silver = 0
   bronze = 0
-
   for each in medalists:
-
-    medal = each[14].stip()
-
+    medal = each[14].strip()
     if medal == 'Gold':
-      golg += 1
+      gold += 1
     elif medal == 'Silwer':
       silver += 1
     elif medal == 'Bronze':
       bronze += 1
+  return gold, silver, bronze
 
-  return golg, silver, bronze
 
-
-def check_valid_country_year(file_name, country, year):
-  country = False
-  year = False
-
-  with open(file_name) as file:
-    next_line = file.readline()
-
-    for line in file:
-      part = line.split('/t')
-      team = part[6].strip()
-      noc = part[7].stip()
-      annum = int(part[9].strip())
-
-      if country in (team, noc):
-        country = True
-      if year in annum:
-        year = True
-
-  return country, year
+def check_valid_country_year(country, year):
+  valid_country = False
+  valid_year = False
+  file = args.infile
+  with open(file) as file:
+        next_line = file.readline()
+        for line in file:
+            row = line.split('\t')
+            team = row[6].strip()
+            noc = row[7].strip()
+            annum = row[9].strip()
+            if country in (team, noc):
+                valid_country = True
+            if year in annum:
+                valid_year = True
+            if valid_country and valid_year:
+                return valid_country, valid_year
+  return valid_country, valid_year
 
 
 def first_ten(medalists):
   ten = []
   for i in range(10):
-    ten.append(f'{medalists[i][1]}-{medalists[i][12]}-{medalists[i][14]}'.strip())
-
+    ten.append(f'{medalists[i][1]} - {medalists[i][12]} - {medalists[i][14]}'.strip())
   return ten
 
 
 country, year = args.medals
+valid_country, valid_year = check_valid_country_year(country, year)
 
-valid_country, valid_year = check_valid_country_year(args.infile, country, year )
-medalists = medalist(args.infile, country, year)
-golg, silver, bronze = total_medals(medalists)
-ten = first_ten(medalists)
-
-if valid_country == False:
+if valid_country == False :
   print("This country doesn't exsist")
-  quit
+  exit()
 
 if valid_year == False:
   print("In this year country didn't take part ")
-  quit
+  exit()
+
+medalists = medalist(country, int(year))
 
 if len(medalists) < 10:
   print(f"In {country} in {year} less than 10 medalists")
-  quit
+  exit()
 
+gold, silver, bronze = total_medals(medalists)
+ten = first_ten(medalists)
+
+def store(ten, gold, silver, bronze):
+    store = f"{ten}\n" \
+            f" Gold medals - {gold}\n " \
+            f"Silver medals - {silver}\n " \
+            f"Bronze medals - {bronze}"
+    return store
+
+
+store = store(ten, gold, silver, bronze)
+print(store)
+
+if args.output is not None:
+  args.output.writelines(f'{store}\n')
